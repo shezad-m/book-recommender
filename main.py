4f7d8cd21ecc5473ed1d159ef20d46f732f65f5f
Shezad <shezadmohamed@gmail.com>
2024-09-03 18:57:39 +0100

    Refactor code

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ recommend a similar one based on past ratings of a wide collection of
 books.
 
 This tool requires three comma separated value files (.csv) named
-Books.csv, Ratings.csv, and Users.csv contained in \dataset.
+Books.csv, Ratings.csv, and Users.csv contained in the dataset folder.
 
 This script requires that `pandas` be installed within the Python
 environment you are running this script in.
@@ -59,21 +59,34 @@ def find_most_similar(ratings: pd.DataFrame, book_id: str) -> str:
         The ISBN of the recommended book
     """
 
-    book_ratings = ratings[(ratings['ISBN'] == book_id) & (ratings['Book-Rating'] > 8)]
-    similar_users = book_ratings['User-ID'].unique()
-    similar_user_recs = ratings[(ratings['User-ID'].isin(similar_users)) & (ratings['Book-Rating'] > 8)]['ISBN']
+    # Get list of books liked by users who liked the input book
+    book_ratings = ratings[(ratings['ISBN'] == book_id)
+                           & (ratings['Book-Rating'] > 8)]
+    sim_users = book_ratings['User-ID'].unique()
+    sim_user_recs = ratings[(ratings['User-ID'].isin(sim_users))
+                            & (ratings['Book-Rating'] > 8)]['ISBN']
 
-    # Get list of books liked by at least 5% of those who liked the input book.
-    similar_user_recs = similar_user_recs.value_counts() / len(similar_users)
-    similar_user_recs = similar_user_recs[similar_user_recs > 0.05]
-    all_users = ratings[(ratings['ISBN'].isin(similar_user_recs.index)) & (ratings['Book-Rating'] > 8)]
-    all_users_recs = all_users['ISBN'].value_counts() / len(all_users['User-ID'].unique())
-    rec_percentages = pd.concat([similar_user_recs, all_users_recs], axis=1)
+    # Get the sublist of books liked by at least 5% of those who liked
+    # the input book.
+    sim_user_recs = sim_user_recs.value_counts() / len(sim_users)
+    sim_user_recs = sim_user_recs[sim_user_recs > 0.05]
+
+    # Out of users with similar tastes, compute the proportion of those
+    # who 
+    all_users = ratings[(ratings['ISBN'].isin(sim_user_recs.index))
+                        & (ratings['Book-Rating'] > 8)]
+    all_users_recs = (all_users['ISBN'].value_counts()
+                      / len(all_users['User-ID'].unique()))
+    rec_percentages = pd.concat([sim_user_recs, all_users_recs],
+                                axis=1)
     rec_percentages.columns = ['Similar', 'All']
-    rec_percentages['Score'] = rec_percentages['Similar'] / rec_percentages['All']
-    rec_percentages = rec_percentages.sort_values('Score', ascending=False)
+    rec_percentages['Score'] = (rec_percentages['Similar']
+                                / rec_percentages['All'])
+    rec_percentages = rec_percentages.sort_values('Score',
+                                                  ascending=False)
 
-    return rec_percentages[rec_percentages.index != book_id].iloc[0].name
+    return rec_percentages[rec_percentages.index
+                           != book_id].iloc[0].name
 
 
 def prep_dataset() -> (pd.DataFrame, pd.DataFrame, pd.DataFrame):
@@ -90,16 +103,18 @@ def prep_dataset() -> (pd.DataFrame, pd.DataFrame, pd.DataFrame):
     ratings = pd.read_csv('dataset/Ratings.csv')
     users = pd.read_csv('dataset/Users.csv')
 
-    # Removes unnecessary columns from the DataFrames and any rows that contain
-    # null values
-    books.drop(columns=['Image-URL-S', 'Image-URL-M', 'Image-URL-L'], inplace=True)
-    books_na = books.index[(books['Book-Author'].isna()) | (books['Publisher'].isna())]
+    # Removes unnecessary columns from the DataFrames and any rows
+    # that contain null values
+    books.drop(columns=['Image-URL-S', 'Image-URL-M', 'Image-URL-L'],
+               inplace=True)
+    books_na = books.index[(books['Book-Author'].isna())
+                           | (books['Publisher'].isna())]
     books.drop(index=books_na, inplace=True)
 
     users.drop(columns=['Age'], inplace=True)
 
-    ratings_not_in_books = ratings.index[~ratings['ISBN'].isin(books['ISBN'])]
-    ratings.drop(index=ratings_not_in_books, inplace=True)
+    ratings_na = ratings.index[~ratings['ISBN'].isin(books['ISBN'])]
+    ratings.drop(index=ratings_na, inplace=True)
 
     return books, ratings, users
 
@@ -113,10 +128,11 @@ def main():
             input_book_name = get_name_from_isbn(books, input_book)
             output_isbn = find_most_similar(ratings, input_book)
             output_name = get_name_from_isbn(books, output_isbn)
-            print(f'If you like "{input_book_name}", then you\'ll like "{output_name}"')
+            out_str = 'If you like "{0}",then you\'ll like "{1}".'
+            out_str = out_str.format(input_book_name, output_name)
         except IndexError:
-            print('That book does not exist in the dataset.')
-            continue
+            out_str = 'That book does not exist in the dataset.'
+        print(out_str)
 
 
 if __name__ == "__main__":
